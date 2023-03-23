f8aef0de5e06a2764f1117399dc5bfb798fc22d7
Kacper Hołowaty <kacper.holowaty@gmail.com>
2023-03-23 20:33:05 +0100

    zadania z lab05

--- lab05/src/main/scala/Zad_03.scala
+++ lab05/src/main/scala/Zad_03.scala
@@ -1,8 +1,6 @@
 package lab05
 
-def chessboard: /*String*/Any = {
-  // val a = (1 to 8).toList
-  // val b = a.
+def chessboard: String = {
   val litery = "abcdefgh"
   val liczby = "12345678".reverse
   val wynik = liczby.flatMap(liczba => litery.map(n => s"($n,$liczba)")).mkString(" ").toString()
